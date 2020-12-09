98196338f3698eae3daa04adac3be03f4a8845a4
stonexp <37534459+stonexp@users.noreply.github.com>
2020-12-09 15:31:50 +0800

    Update AutoUpdate.sh

--- Customize/AutoUpdate.sh
+++ Customize/AutoUpdate.sh
@@ -5,7 +5,7 @@
 
 Version=V4.2
 DEFAULT_DEVICE=d-team_newifi-d2
-Github=https://github.com/Hyy2001X/AutoBuild-Actions
+Github=https://github.com/stonexp/AutoBuild-Actions
 
 TIME() {
 	echo -ne "\n[$(date "+%H:%M:%S")] "
